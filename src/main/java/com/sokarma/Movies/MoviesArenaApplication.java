89cdf0d179e7d4273e77b54f3debcf702141069a
souvik943 <souvikkarmakar2k19@gmail.com>
2024-06-26 20:03:53 +0530

    Initial Commit

---
+++ src/main/java/com/sokarma/Movies/MoviesArenaApplication.java
@@ -0,0 +1,13 @@
+package com.sokarma.Movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesArenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesArenaApplication.class, args);
+	}
+
+}
